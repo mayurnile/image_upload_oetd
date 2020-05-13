446d59c354df55c919e99474b9895eb0b0bd75ad
Mayur Nile <mayurnile95@gmail.com>
2020-05-13 14:57:00 +0530

    first commit

---
+++ lib/screens/home_page.dart
@@ -0,0 +1,66 @@
+import 'dart:io';
+import 'dart:convert';
+
+import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
+import 'package:http/http.dart' as http;
+
+class HomeScreen extends StatefulWidget {
+  @override
+  _HomeScreenState createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  final String serverEndPoint = "";
+
+  File file;
+
+  void _choose() async {
+    file = await ImagePicker.pickImage(source: ImageSource.camera);
+    // file = await ImagePicker.pickImage(source: ImageSource.gallery);
+    setState(() {});
+  }
+
+  void _upload() {
+    if (file == null) return;
+    String base64Image = base64Encode(file.readAsBytesSync());
+    String fileName = file.path.split("/").last;
+
+    http.post(serverEndPoint, body: {
+      "image": base64Image,
+      "name": fileName,
+    }).then((res) {
+      print(res.statusCode);
+    }).catchError((err) {
+      print(err);
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Image Uploader'),
+      ),
+      body: Column(
+        children: <Widget>[
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              RaisedButton(
+                onPressed: _choose,
+                child: Text('Choose Image'),
+              ),
+              SizedBox(width: 10.0),
+              RaisedButton(
+                onPressed: _upload,
+                child: Text('Upload Image'),
+              )
+            ],
+          ),
+          file == null ? Text('No Image Selected') : Image.file(file)
+        ],
+      ),
+    );
+  }
+}
